f32d03c2079fa621094e9cf9ff3276f0299042ab
a0548507128 <a0548507128@gmail.com>
2022-10-26 19:47:38 +0300

    Initial code + Welcome functions

---
+++ Stage0/Program0838.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Transactions;
+namespace stage0
+{
+     partial class program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0468();
+            Welcome0838();
+            Console.ReadKey();
+        }
+        static partial void Welcome0468();
+        private static void Welcome0838()
+        {
+            string name;
+            Console.WriteLine("Enter your name:");
+            name = Console.ReadLine();
+            Console.WriteLine(name + " welcome to my first console application");
+        }
+    }
+}
