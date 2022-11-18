464eec2982380c7db2f987b5378d21db93afc21b
a0548507128 <a0548507128@gmail.com>
2022-11-18 13:19:14 +0200

    stage2-1.1

---
+++ DalFacade/DalApi/ICrud.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalApi
+{
+    public interface ICrud<T>
+    {
+        public T Add (T IdAdd);
+        public T Update (T IdUpdate);
+        public T Delete (T IdDelete);
+        public T Get (int IdGet);
+
+    }
+}
