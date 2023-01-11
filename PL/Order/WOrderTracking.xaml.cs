1f5c3bbe3efacf7d244a9b692a56720e4f81428d
a0548507128 <a0548507128@gmail.com>
2023-01-11 23:17:15 +0200

    stage5-1.8

--- PL/Order/WOrderTracking.xaml.cs
+++ PL/Order/WOrderTracking.xaml.cs
@@ -22,6 +22,15 @@ namespace PL
     public partial class WOrderTracking : Window
     {
         BlApi.IBl? bl = BlApi.Factory.Get();
+       // public Visibility IsVisible1 { get; set; }=Visibility.Collapsed;
+        public Visibility IsVisible1
+        {
+            get { return (Visibility)GetValue(IsVisible1Property); }
+            set { SetValue(IsVisible1Property, value); }
+        }
+        public static readonly DependencyProperty IsVisible1Property = DependencyProperty.Register(nameof(IsVisible1),
+                                                                                                        typeof(Visibility),
+                                                                                                       typeof(WOrderTracking));
         public int OrderId { get; set; } = 0;
         public BO.OrderTracking OrderTracking1
         {
@@ -33,14 +42,19 @@ namespace PL
                                                                                                        typeof(WOrderTracking));
         public WOrderTracking()
         {
+            IsVisible1=Visibility.Collapsed;
             InitializeComponent();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            IsVisible1=Visibility.Visible;
             if (bl != null) OrderTracking1 =  bl.Order.OrderTracking(OrderId);
-          
         }
 
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            new WOrderDetails(OrderId).ShowDialog();
+        }
     }
 }
