9c8cb4c5446651716f9413fc1fe3b17027d2b143
a0548507128 <a0548507128@gmail.com>
2023-02-17 03:13:16 +0200

    stage6-1.2

---
+++ DalXml/Order.cs
@@ -0,0 +1,37 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    internal class Order : IOrder
+    {
+        public int Add(DO.Order IdAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int IdDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DO.Order? Get(int IdGet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DO.Order?> GetAll(Predicate<DO.Order?>? predict = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(DO.Order IdUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DalXml/Product.cs
@@ -0,0 +1,37 @@
+﻿using DalApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dal
+{
+    internal class Product : IProduct
+    {
+        public int Add(DO.Product IdAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int IdDelete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DO.Product? Get(int IdGet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<DO.Product?> GetAll(Predicate<DO.Product?>? predict = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(DO.Product IdUpdate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
